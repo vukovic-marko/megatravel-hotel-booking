c7f4872ae20b4fa7fc9fdbdb616eff00d1832622
Marko <vukovic.marko171@gmail.com>
2019-06-13 17:39:46 +0200

    Manja izmena

--- agent/src/main/java/tim23/agent/controller/HotelController.java
+++ agent/src/main/java/tim23/agent/controller/HotelController.java
@@ -2,17 +2,32 @@ package tim23.agent.controller;
 
 import javax.websocket.server.PathParam;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import hello.wsdl.GetSobaResponse;
 import hello.wsdl.Soba;
+import tim23.agent.SobaClient;
 
 @RestController
 public class HotelController {
 
+	@Autowired
+	private SobaClient quoteClient;
+	
+	@GetMapping("/test")
+	public Soba test() {
+		int soba = 4;
+
+		GetSobaResponse response = quoteClient.get(soba);
+		
+		return response.getSoba();
+	}
+	
 	@GetMapping("/room/{id}")
 	public Soba getRoom(@PathParam("id") long id) {
 		
