0a54ebfbc93b2721079eb42ff7ceb74979a5e6fd
Marko <vukovic.marko171@gmail.com>
2019-06-16 16:06:12 +0200

    Ispravljena lozinka za agenta

--- glavna/hotel-service/src/main/resources/import.sql
+++ glavna/hotel-service/src/main/resources/import.sql
@@ -14,7 +14,7 @@ insert into authority (id, name) values (3, 'KRAJNJI');
 
 
 -- lozinka: admin
-insert into administrator (id_korisnika, email, lozinka, ime, prezime, uloga) values (1, 'admin', 'lgJQcCz05yTeNzg0SKjTcO3Nku07zTf6KscCBud5PKUx3PgmJ5SPq', 'Admin', 'Admin', 0);
+insert into administrator (id_korisnika, email, lozinka, ime, prezime, uloga) values (1, 'admin', '$2a$04$1B8pqTQWYCGcnYQ/MusPROWXKthEV3dFEgnqInbNg2P9Ka9DSKICG', 'Admin', 'Admin', 0);
 insert into administrator_authority (user_id, authority_id) values (1, 1);
 
 -- lozinka: agent
