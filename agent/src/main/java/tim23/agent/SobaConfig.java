9f1cb25efa5379da021279c67213d1ae50375688
Marko <vukovic.marko171@gmail.com>
2019-06-18 15:10:23 +0200

    Ispravljen SOAP endpoint

--- agent/src/main/java/tim23/agent/SobaConfig.java
+++ agent/src/main/java/tim23/agent/SobaConfig.java
@@ -4,24 +4,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
-@Configuration
+//@Configuration
 public class SobaConfig {
 	
-	@Bean
-	public Jaxb2Marshaller marshaller() {
-		Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
-		// this package must match the package in the <generatePackage> specified in
-		// pom.xml
-		marshaller.setContextPath("hello.wsdl");
-		return marshaller;
-	}
-	
-	@Bean
-	public SobaClient countryClient(Jaxb2Marshaller marshaller) {
-		SobaClient client = new SobaClient();
-		client.setDefaultUri("http://localhost:8762/hotel-service/ws");
-		client.setMarshaller(marshaller);
-		client.setUnmarshaller(marshaller);
-		return client;
-	}
+//	@Bean
+//	public Jaxb2Marshaller marshaller() {
+//		Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+//		// this package must match the package in the <generatePackage> specified in
+//		// pom.xml
+//		marshaller.setContextPath("hello.wsdl");
+//		return marshaller;
+//	}
+//	
+//	@Bean
+//	public SobaClient countryClient(Jaxb2Marshaller marshaller) {
+//		SobaClient client = new SobaClient();
+//		client.setDefaultUri("http://localhost:8762/hotel-service/ws");
+//		client.setMarshaller(marshaller);
+//		client.setUnmarshaller(marshaller);
+//		return client;
+//	}
 }
