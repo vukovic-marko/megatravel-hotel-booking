395ea540ae1c72c07ddb5107ef3079691b213e29
Marko Vukovic <44498569+vukovic-marko@users.noreply.github.com>
2019-06-23 21:14:58 +0200

    Manja izmena

--- glavna/admin-service/src/main/resources/static/js/agent.js
+++ glavna/admin-service/src/main/resources/static/js/agent.js
@@ -1,28 +1,29 @@
+$(document).ready(function() {
+	$('#formaagentreg').submit(function (e) {
+		e.preventDefault();
+		
+		var username = document.getElementById("username").value;
 
-function addNewAgentForm(){
-
-	var username = document.getElementById("username").value;
-
-	$.ajax({
-		url: "http://localhost:8762/admin-service/admin/register/"+username,
-	    type:"GET",
-	    
-	   contentType: 'application/json; charset=utf-8',
-	    success: function (data){
-	    	if(data==false)
-	    	upisiAgenta();
-	    	else if(data==true)
-	    		alert("POSTOJI");
-	    	else 
-	    		alert(data);
-	    },
-	    error: function(){
-	    	alert("pukao ovde ");
-	    }
+		$.ajax({
+			url: "http://localhost:8762/admin-service/admin/register/"+username,
+		    type:"GET",
+		    
+		   contentType: 'application/json; charset=utf-8',
+		    success: function (data){
+		    	if(data==false)
+		    	upisiAgenta();
+		    	else if(data==true)
+		    		alert("POSTOJI");
+		    	else 
+		    		alert(data);
+		    },
+		    error: function(){
+		    	alert("pukao ovde ");
+		    }
+		});
 	});
-	
-	
-}
+});
+
 function upisiAgenta(){
 	var ime = document.getElementById("name").value;
 	var prezime = document.getElementById("surname").value;
@@ -39,8 +40,7 @@ function upisiAgenta(){
 		url: "http://localhost:8762/admin-service/admin/register",
 		type: "POST",
 		data:agent,
-		dataType:"json",
-		   contentType: 'application/json; charset=utf-8',
+		contentType: 'application/json',
 		success: function(){
 			alert("Uspesno dodat");
 			window.location.reload();
